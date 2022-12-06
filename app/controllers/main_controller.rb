43b454d7dc3e4f169a1a8952fee7e1fa4652fbdc
DylanBrennan92 <x18119859@student.ncirl.ie>
2022-12-06 14:24:10 +0000

    filtered flash msgs

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class MainController < ApplicationController
     def index
         flash[:notice] = "Logged in successfully"
         flash[:alert] = "Invalid email or password"
