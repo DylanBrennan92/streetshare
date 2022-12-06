3f2925dc0b23773fb94df21660493a3d35b0764d
DylanBrennan92 <x18119859@student.ncirl.ie>
2022-12-06 15:43:07 +0000

    just before changeover from sqlite3 db to postgresql

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
     def index
         flash[:notice] = "Logged in successfully"
         flash[:alert] = "Invalid email or password"
+        @secret = ENV['DATABASE_PASSWORD']
     end
 end
