44effd8a4bf88fdcc05e41f4408cf9e3e729ea1c
DylanBrennan92 <x18119859@student.ncirl.ie>
2022-12-10 12:55:49 +0000

    schema modified before heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_181846) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_08_130635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
